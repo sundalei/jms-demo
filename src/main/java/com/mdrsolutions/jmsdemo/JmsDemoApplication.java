94b90f3b313b9199d727880c738fc291312824b3
DaLei Sun <sundalei1988@gmail.com>
2022-08-03 09:22:15 +0800

    run in web container

--- src/main/java/com/mdrsolutions/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/mdrsolutions/jmsdemo/JmsDemoApplication.java
@@ -2,11 +2,18 @@ package com.mdrsolutions.jmsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.jms.annotation.EnableJms;
 
 @EnableJms
 @SpringBootApplication
-public class JmsDemoApplication {
+public class JmsDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JmsDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JmsDemoApplication.class, args);
