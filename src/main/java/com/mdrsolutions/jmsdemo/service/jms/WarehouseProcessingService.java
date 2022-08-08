84d7fe60778f6d50a8ca152e7c6af167cf193e8d
DaLei Sun <sundalei1988@gmail.com>
2022-08-08 15:20:31 +0800

    message header customization.

--- src/main/java/com/mdrsolutions/jmsdemo/service/jms/WarehouseProcessingService.java
+++ src/main/java/com/mdrsolutions/jmsdemo/service/jms/WarehouseProcessingService.java
@@ -23,8 +23,29 @@ public class WarehouseProcessingService {
     }
 
     @Transactional
-    public void processOrder(BookOrder bookOrder) {
+    public void processOrder(BookOrder bookOrder, String orderState, String storeId) {
         ProcessedBookOrder order = new ProcessedBookOrder(bookOrder, new Date(), new Date());
+
+        if ("NEW".equalsIgnoreCase(orderState)) {
+            add(bookOrder, storeId);
+        } else if ("UPDATE".equalsIgnoreCase(orderState)) {
+            update(bookOrder, storeId);
+        } else if ("DELETE".equalsIgnoreCase(orderState)) {
+            delete(bookOrder, storeId);
+        }
+
         jmsTemplate.convertAndSend("book.order.processed.queue", order);
+    }
+
+    private void add(BookOrder bookOrder, String storeId) {
+        LOGGER.info("ADDING A NEW ORDER TO THE DB");
+    }
+
+    private void update(BookOrder bookOrder, String storeId) {
+        LOGGER.info("UPDATING AN ORDER TO THE DB");
+    }
+
+    private void delete(BookOrder bookOrder, String storeId) {
+        LOGGER.info("DELETING THE ORDER FROM DB");
     }
 }
--- src/main/java/com/mdrsolutions/jmsdemo/service/jms/WarehouseReceiverService.java
+++ src/main/java/com/mdrsolutions/jmsdemo/service/jms/WarehouseReceiverService.java
@@ -4,6 +4,9 @@ import com.mdrsolutions.jmsdemo.pojos.BookOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +21,19 @@ public class WarehouseReceiverService {
     }
 
     @JmsListener(destination = "book.order.queue", containerFactory = "defaultJmsListenerContainerFactory")
-    public void receive(BookOrder bookOrder) {
+    public void receive(@Payload BookOrder bookOrder,
+                        @Header("orderState") String orderState,
+                        @Header("bookOrderId") String bookOrderId,
+                        @Header("storeId") String storeId,
+                        MessageHeaders messageHeaders) {
         LOGGER.info("received a message");
         LOGGER.info("Message is: " + bookOrder);
+        LOGGER.info("Message property orderState = {}, bookOrderId = {}, storeId = {}", orderState, bookOrderId, storeId);
+        LOGGER.info("messageHeaders = {}", messageHeaders);
 
         if (bookOrder.getBook().getTitle().startsWith("L")) {
             throw new RuntimeException("OrderId=" + bookOrder.getBookOrderId() + " begins with L and these books are not allowed.");
         }
-        warehouseProcessingService.processOrder(bookOrder);
+        warehouseProcessingService.processOrder(bookOrder, orderState, storeId);
     }
 }
